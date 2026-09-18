e6b8b4cb1ac2f67d1cd61fc296d1792a7b0a9dcb
Allen <allenliu@outlook.com>
2026-09-18 09:20:52 -0500

    Initial Commit

---
+++ CafeMini/CafeMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeMiniApp.swift
+//  CafeMini
+//
+//  Created by ALLEN LIU on 9/18/26.
+//
+
+import SwiftUI
+
+@main
+struct CafeMiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
